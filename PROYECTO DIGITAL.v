25d651b6e42e86af92456fe3968d7e767fceaa4a
Palo <pjestrada006@hotmail.com>
2017-10-23 13:49:10 -0600

    Started inmediates.

--- PROYECTO DIGITAL.v
+++ PROYECTO DIGITAL.v
@@ -19,14 +19,14 @@ endmodule
 module flash_program_memory(
   input clk,
   input [11:0] addr,
-  output [11:0]instruction
+  output [13:0]instruction
 );
 	/*
 		Este modulo se encarga de leer una nueva instruccion de la memoria
 		en cada cambio de reloj y lo coloca en el output instruction.
 	*/
-	reg [11:0] instruction;
-    reg [11:0] memory [0:1024]; // Este numero debe cambiar dependiendo el tamaño del archivo
+	reg [13:0] instruction;
+    reg [13:0] memory [0:1024]; // Este numero debe cambiar dependiendo el tamaño del archivo
 
 
 	always @(posedge clk) begin
@@ -40,98 +40,149 @@ module flash_program_memory(
 
 endmodule
 
-module alu_version_1(input clk, input[3:0] opcode,
+module alu_version_1(input clk, input[3:0] opcode, input[1:0] type_opcode,
  input[7:0] w_reg, input[7:0] f_reg, output[7:0] result);
 
 	reg[7:0] result;
 	always @(posedge clk) begin
-		case (opcode)
-		0: begin
-		// MOVWF
-			result <= w_reg;
+		if(type_opcode == 0)begin
+			case (opcode)
+				0: begin
+				// MOVWF
+					result <= f_reg;
+				end
+				1: begin
+				// CLRF
+					result <= 0;
+				end
+				2: begin
+				// SUBWF
+					result <= f_reg - w_reg; 
+				end
+				3: begin
+				// DECF
+					result <= f_reg - 1; 
+				end
+				4: begin
+				// IORWF
+					result <= f_reg || w_reg; 
+				end
+				5: begin
+				// AND
+					result <= f_reg && w_reg; 
+				end
+				6: begin
+				// XORWF
+					result <= f_reg ^ w_reg; 
+				end
+				7: begin
+				// ADDWF
+					result <= f_reg + w_reg; 
+				end
+				8: begin
+				// MOVF
+					result <= f_reg; 
+				end
+				9: begin
+				// COMF
+					result <= ~f_reg; 
+				end
+				10: begin
+				// INCF
+					result <= f_reg + 1; 
+				end
+				11: begin
+				// MOVF
+					//TODO: Pendiente el JUMP
+					result <= f_reg - 1 ; 
+				end
+				12: begin
+				// RLF
+					//TODO: Tomar en cuenta la carry flag.
+					result <=  f_reg << 0 ; 
+				end
+				13: begin
+				// RRF
+					result <= f_reg >> 0 ; 
+				end
+				14: begin
+				// SWAPF
+					result[3:0] <= f_reg[7:4];
+					result[7:4] <= f_reg[3:0]; 
+				end
+				15: begin
+				// INCFSZ
+					//TODO: Pendiente la ejecucion del NOP (Doble ciclo)
+					result <= f_reg + 1; 
+				end													
+	        endcase
 		end
-		1: begin
-		// CLRF
-			result <= 0;
+		// BIT Instructions
+		else if (type_opcode == 1) begin
+			
 		end
-		2: begin
-		// SUBWF
-			result <= f_reg - w_reg; 
+		// CALL / GOTO
+		else if (type_opcode == 2) begin
+			
 		end
-		3: begin
-		// DECF
-			result <= f_reg - 1; 
+		// INMEDIATES
+		else if (type_opcode == 3) begin
+			// MOVLW
+			if (opcode[3]==0 && opcode[2]==0) begin
+				result <= f_reg
+			end
+			// RETLW
+			else if (opcode[3]==0 && opcode[2]==1) begin
+				//TODO: Pendiente
+			end
+			// IORLW
+			else if ( opcode == 8) begin
+				result <= w_reg || f_reg
+			end
+			// ANDLW
+			else if ( opcode == 9) begin
+				result <= w_reg && f_reg
+			end
+			// XORLW
+			else if ( opcode == 9) begin
+				result <= w_reg && f_reg
+			end
+			// ANDLW
+			else if ( opcode == 9) begin
+				result <= w_reg ^ f_reg
+			end		
+			// XORWLW
+			else if ( opcode == 10) begin
+				result <= w_reg ^ f_reg
+			end	
+			// SUBLW
+			else if (opcode[3]==1&&opcode[2]==1&&opcode[1]==0) begin
+				result <= f_reg - w_reg
+			end	
+			// ADDLW
+			else if (opcode[3]==1&&opcode[2]==1&&opcode[1]==1) begin
+				result <= f_reg + w_reg
+			end						
 		end
-		4: begin
-		// IORWF
-			result <= f_reg || w_reg; 
-		end
-		5: begin
-		// AND
-			result <= f_reg && w_reg; 
-		end
-		6: begin
-		// XORWF
-			result <= f_reg ^ w_reg; 
-		end
-		7: begin
-		// ADDWF
-			result <= f_reg + w_reg; 
-		end
-		8: begin
-		// MOVF
-			result <= f_reg; 
-		end
-		9: begin
-		// COMF
-			result <= ~f_reg; 
-		end
-		10: begin
-		// INCF
-			result <= f_reg + 1; 
-		end
-		11: begin
-		// MOVF
-			//TODO: Pendiente el JUMP
-			result <= f_reg - 1 ; 
-		end
-		12: begin
-		// RLF
-			//TODO: Tomar en cuenta la carry flag.
-			result <=  f_reg << 0 ; 
-		end
-		13: begin
-		// RRF
-			result <= f_reg >> 0 ; 
-		end
-		14: begin
-		// SWAPF
-			result[3:0] <= f_reg[7:4];
-			result[7:4] <= f_reg[3:0]; 
-		end
-		15: begin
-		// INCFSZ
-			//TODO: Pendiente la ejecucion del NOP (Doble ciclo)
-			result <= f_reg + 1; 
-		end													
-        endcase
+
 	end
 
 endmodule
 
 module instruction_register(input clk,
-                            input [11:0]current_instr,
-                            output [3:0] opcode,
+                            input [13:0]current_instr,
+                            output [5:0] full_opcode,
                             output [6:0] register_address,
-                            output enable);
+                            output enable,
+                            output inmediate_enable);
 
-	assign opcode = current_instr[11:8];
+	assign full_opcode = current_instr[13:8];
 
 	// Usualmente son 7 bits para el register address porque hay uno para el bit "d".
 	// TODO: Agregar logica para cuando las instrucciones
 	//		no son con bit d. (Ejemplo: inmediatos)
 	assign register_address = current_instr[6:0];
-	
+	assign inmediate_enable =  current_instr[13];
 	assign enable = current_instr[7];
 
 endmodule
@@ -179,17 +230,26 @@ module w_register(input clk, input enable_write,
     	outdata = 0;
     end
 endmodule
-/*
-module mux_inmediate(input enable, input[7:0] inm, input[7:0] registers_out,
+
+module mux_inmediate(input clk, input enable, input[7:0] inm, input[7:0] registers_out,
 	output[7:0] out);
-	if(enable)begin
-		assign out = inm;
-	end
-	else begin
-		assign out = registers_out;
+	reg[7:0] out;
+	always @(posedge clk) begin
+		if(enable)begin
+			out <= inm;
+		end
+		else begin
+			out <= registers_out;
+		end
 	end
 endmodule
-*/
+
+module decode(input[5:0] full_opcode, output[1:0] type_opcode,
+				output[3:0] opcode);
+
+	assign  type_opcode =  full_opcode[5:4];
+	assign  opcode =  full_opcode[3:0];
+endmodule
 module clock_divisor(input clk,
                       output clk0_out,
                       output clk1_out,
--- testbench.v
+++ testbench.v
@@ -5,27 +5,32 @@ module pic();
 	reg [11:0]pc;
 	wire clk_pc, clk_instruction_memory, clk_alu, clk_registers;
 
-	wire [11:0] current_instruction;
+	wire [13:0] current_instruction;
+	wire [5:0] full_opcode;
+	wire[1:0] type_opcode;
 	wire [3:0] opcode;
+	wire inmediate_enable;
 	wire [6:0] register_address;
 	wire enable_instr_reg;
 	wire [11:0] instruction_reg_out;
 	wire [7:0] registers_out;
 	wire [7:0] w_reg_out;
-  	wire [7:0] alu_out;
+  	wire [7:0] alu_out, alu_in;
 
 	pc pc_pic (clk_pc, reset, pc);
 	flash_program_memory instruction_memory (clk_instruction_memory, pc, current_instruction);
 	// El instruction register hace forward de la instruccion 
-  	instruction_register instruction_register(clk_instruction_memory, current_instruction, opcode, 
-  		register_address , enable_instr_reg);
+  	instruction_register instruction_register(clk_instruction_memory, current_instruction, full_opcode, 
+  		register_address , enable_instr_reg, inmediate_enable);
  
  
   	w_register w_register (clk_registers, ~enable_instr_reg, alu_out, w_reg_out);
     
     registers registers(clk_registers, enable_instr_reg, register_address, alu_out, registers_out);
 
-  	alu_version_1 alu (clk_alu, opcode, w_reg_out, registers_out, alu_out);
+    mux_inmediate mux_inmediate(clk_instruction_memory, inmediate_enable, register_address, registers_out, alu_in);
+  	decode decode(full_opcode, type_opcode, opcode);
+  	alu_version_1 alu (clk_alu, opcode, type_opcode,  w_reg_out, alu_in, alu_out);
 
 	
 	// Clock Management
@@ -55,7 +60,7 @@ module pic();
     end
   
 	initial
- 	 #500 $finish;
+ 	 #1000 $finish;
   
 
 	always
